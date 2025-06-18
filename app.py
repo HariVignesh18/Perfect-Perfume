811c837fc0d9840b63c208c897d7f197453eacd1
HariVignesh18 <harivirat2005@gmail.com>
2025-06-18 20:03:04 +0530

    Removed SSL

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ def get_db_connection():
         host=os.getenv('DB_HOST'),
         user=os.getenv('DB_USERNAME'),
         password=os.getenv('DB_PASSWORD'),
-        database=os.getenv('DB_DBNAME'),
-        ssl_ca=os.getenv('DB_SSL_CA')
+        database=os.getenv('DB_DBNAME')
     )
 
 
